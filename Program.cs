9c13be4fa5be425a3ef5676dcc41bc4d369a5c3f
Eetu <isoananas@gmail.com>
2019-06-12 12:11:16 +0300

    Ekan koodin lisays

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace korkoakorolle
+{
+    class Program
+    {
+
+        static void Alkupaaoma()
+        {
+             int alkupaaoma = 0;
+             Console.WriteLine("Syötä alkupääoma:");
+            alkupaaoma = int.Parse(Console.ReadLine());
+        }
+
+        static void Korko()
+        {
+             int korko = 0;
+             Console.WriteLine("Syötä vuosikorko:");
+            korko = int.Parse(Console.ReadLine());
+        }
+
+          static void Sijoitusaika()
+        {
+             int sijoitusaika = 0;
+             Console.WriteLine("Syötä sijoitusaika vuosina:");
+            sijoitusaika = int.Parse(Console.ReadLine());
+        }
+
+          static void Kuukausisijoitus()
+        {
+             int kksijoitus = 0;
+             Console.WriteLine("Syötä vuosikorko:");
+            kksijoitus = int.Parse(Console.ReadLine());
+        }
+
+
+        static void Main(string[] args)
+        {
+            Alkupaaoma();
+            Korko();
+            Sijoitusaika();
+            Kuukausisijoitus();
+        }
+    }
+}
