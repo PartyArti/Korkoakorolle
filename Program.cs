33c3cf0e83f9fa06a1ee5e2431b162153a63da1c
PartyArti <isoananas@gmail.com>
2019-09-17 20:09:08 +0300

    Update Program.cs
    Toimii, kk-sijoitusta ei vielä lisätty

--- Program.cs
+++ Program.cs
@@ -2,44 +2,56 @@
 
 namespace korkoakorolle
 {
-    class Program
+        class Program
     {
 
-        static void Alkupaaoma()
+        static double Alkupaaoma(double a)
         {
-             int alkupaaoma = 0;
+             double alkupaaoma = 0;
              Console.WriteLine("Syötä alkupääoma:");
-            alkupaaoma = int.Parse(Console.ReadLine());
+            alkupaaoma = double.Parse(Console.ReadLine());
+            return alkupaaoma;
         }
 
-        static void Korko()
+        static double Korko(double b)
         {
-             int korko = 0;
-             Console.WriteLine("Syötä vuosikorko:");
-            korko = int.Parse(Console.ReadLine());
+             double korko = 0;
+             Console.WriteLine("Syötä vuosikorko prosentteina:");
+            korko = double.Parse(Console.ReadLine());
+            korko = korko*0.01;
+            return korko;
         }
 
-          static void Sijoitusaika()
+          static double Sijoitusaika(double c)
         {
-             int sijoitusaika = 0;
+             double sijaika = 0;
              Console.WriteLine("Syötä sijoitusaika vuosina:");
-            sijoitusaika = int.Parse(Console.ReadLine());
+            sijaika = double.Parse(Console.ReadLine());
+            return sijaika;
         }
 
-          static void Kuukausisijoitus()
+          static double Kuukausisijoitus(double d)
         {
-             int kksijoitus = 0;
-             Console.WriteLine("Syötä vuosikorko:");
-            kksijoitus = int.Parse(Console.ReadLine());
+             double kksij = 0;
+             Console.WriteLine("Syötä Kuukausisijoitus:");
+            kksij = double.Parse(Console.ReadLine());
+            return kksij;
         }
 
 
         static void Main(string[] args)
         {
-            Alkupaaoma();
-            Korko();
-            Sijoitusaika();
-            Kuukausisijoitus();
+          double a=0,b=0,c=0,tuotto=0;
+          //double a=0,b=0,c=0,d=0,tuotto=0;
+          double alkupaaoma = Alkupaaoma(a);
+          double korko =Korko(b);
+          double sijaika = Sijoitusaika(c);
+          //double kksij = Kuukausisijoitus(d);
+
+            //K (t) = k(0)x(1+p)^t
+            tuotto = alkupaaoma*(Math.Pow((1 + korko),sijaika));
+            Console.WriteLine("Tuotto on: ");
+            Console.WriteLine(tuotto);
         }
     }
 }
